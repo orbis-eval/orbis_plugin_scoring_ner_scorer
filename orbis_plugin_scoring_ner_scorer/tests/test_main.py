4425db63914c114b6593a56cdd210f8381f69963
fabod <2189668+fabod@users.noreply.github.com>
2020-08-08 15:20:26 +0200

    fix: gitignore, no more creating testdata, correct source.txt in rss1

---
+++ orbis_plugin_scoring_ner_scorer/tests/test_main.py
@@ -0,0 +1,18 @@
+def test_run():
+    assert False
+
+
+def test_get_scored():
+    assert False
+
+
+def test_calc_score():
+    assert False
+
+
+def test_get_unscored():
+    assert False
+
+
+def test_get_confusion_matrix():
+    assert False
